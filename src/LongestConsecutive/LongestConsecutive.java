4864ff22173469f44fba3036378ca8d645c8919d
rshanEmm <72631559+rshanEmm@users.noreply.github.com>
2020-10-20 21:37:22 -0400

    Update LongestConsecutive.java
    Cleaning up the comments

--- src/LongestConsecutive/LongestConsecutive.java
+++ src/LongestConsecutive/LongestConsecutive.java
@@ -20,12 +20,6 @@ public class LongestConsecutive {
         resetLongest();
         List<String> hold = new ArrayList();
 
-        /*
-        This is something like O(n(n as along as n as a pattern)), this was as close to O(n) as I could get.
-        At least it not true O(n^2). I did use contain and indexOf to position the second loop in a logical fashion.
-        It breaks out on a bad condition statement then moves on to the next position in first after checking for
-        a save condition.
-         */
         for (int i=0; i<first.size(); i++) {
             // Finding the string contained in the
             if(!second.contains(first.get(i))){
@@ -41,11 +35,7 @@ public class LongestConsecutive {
                 hold = new ArrayList();
                 continue;
             }
-            /*
-            The bane of this, wish I had a solution where I did not have to setup an inner loop yet I do not have time.
-            Found out things I have to take care of so I settled on a slightly less then O(n) solution. With how
-            this test was designed, I could not use a "Set" and O(n) anyway.
-             */
+
             for(int j = jLoopSetter; j < second.size(); ++j ) {
 
 
