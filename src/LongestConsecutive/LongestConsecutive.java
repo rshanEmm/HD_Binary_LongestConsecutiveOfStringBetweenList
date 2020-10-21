de913dee93ed3cd236745672e81a97be9bb27e6e
rshanEmm <72631559+rshanEmm@users.noreply.github.com>
2020-10-20 22:22:41 -0400

    Update LongestConsecutive.java
    stylecheck

--- src/LongestConsecutive/LongestConsecutive.java
+++ src/LongestConsecutive/LongestConsecutive.java
@@ -6,37 +6,40 @@ import java.util.List;
 public class LongestConsecutive {
     private List<String> longest = new ArrayList();
 
-    public List<String> getLongest() {return longest;}
+    public List<String> getLongest() {
+        return longest;
+    }
 
-    private void resetLongest(){longest = new ArrayList();}
+    private void resetLongest() {
+        longest = new ArrayList();
+    }
 
-    private void checkLongest(List hold){
-        if(longest.size() < hold.size()) {
+    private void checkLongest(final List hold) {
+        if (longest.size() < hold.size()) {
             this.longest = hold;
         }
     }
 
-    public void contains(List first, List second) {
+    public void contains(final List first, final List second) {
         resetLongest();
         List<String> hold = new ArrayList();
 
-        for (int i=0; i<first.size(); i++) {
+        for (int i = 0; i < first.size(); i++) {
             // Finding the string contained in the
-            if(!second.contains(first.get(i))){
+            if (!second.contains(first.get(i))) {
                 continue;
             } else {
-                hold.add((String)second.get(second.indexOf(first.get(i))));
+                hold.add((String) second.get(second.indexOf(first.get(i))));
             }
-            int iTemp = i+1;
-            int jLoopSetter = second.indexOf(first.get(i))+1;
-            // if string found at last position of second list then save, reset, try again
-            if(jLoopSetter >= second.size()){
+            int iTemp = i + 1;
+            int jLoopSetter = second.indexOf(first.get(i)) + 1;
+            if (jLoopSetter >= second.size()) {
                 checkLongest(hold);
                 hold = new ArrayList();
                 continue;
             }
 
-            for(int j = jLoopSetter; j < second.size(); ++j ) {
+            for (int j = jLoopSetter; j < second.size(); ++j) {
 
 
                 if (first.get(iTemp) == second.get(j)) {
